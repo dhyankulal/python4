9394a625384b1ffa735b8227f3641dcfa185102c
Dhyan kulal <dhyankulal47@gmail.com>
2026-03-12 11:15:33 +0530

    mathematical table

---
+++ 2.py
@@ -0,0 +1,12 @@
+print("2*1=2")
+print("2*2=4")
+print("2*3=6")
+print("2*4=8")
+print("2*5=10")
+
+for i in range(1,10,1):
+    print(2,"*",i,"=",2*i)
+
+for j in range(1,3,1):
+    for i in range(1,11,1):
+        print(j,"*",i,"=",j*i)
